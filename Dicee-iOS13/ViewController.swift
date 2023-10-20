6a9fbf5a065fb7f374d9068a61828552fd71e80d
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-20 15:16:08 +0500

    First steps

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = UIImage(named: "DiceSix")
+        diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.image = UIImage(named: "DiceTwo")
     }
 
 
